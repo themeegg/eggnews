3c0e07a296a5a57651c77e86948e4c10826105f1
Theme Egg <themeeggofficial@gmail.com>
2017-09-02 21:30:57 +0545

    website skin added

--- inc/admin/assets/eggnews-sanitize.php
+++ inc/admin/assets/eggnews-sanitize.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Define function about sanitation for customizer option
- * 
+ *
  * @package Theme Egg
  * @subpackage Eggnews
  * @since 1.0.0
@@ -9,115 +9,128 @@
 
 //Text
 function eggnews_sanitize_text( $input ) {
-    return wp_kses_post( force_balance_tags( $input ) );
+	return wp_kses_post( force_balance_tags( $input ) );
 }
 
 //Check box
 function eggnews_sanitize_checkbox( $input ) {
-    if ( $input == 1 ) {
-        return 1;
-    } else {
-        return 0;
-    }
+	if ( $input == 1 ) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 // Number
 function eggnews_sanitize_number( $input ) {
-    $output = intval($input);
-     return $output;
+	$output = intval( $input );
+
+	return $output;
 }
 
 // site layout
-function eggnews_sanitize_site_layout($input) {
-    $valid_keys = array(
-        'fullwidth_layout' => __( 'Fullwidth Layout', 'eggnews' ),
-        'boxed_layout' => __( 'Boxed Layout', 'eggnews' )
-        );
-    if ( array_key_exists( $input, $valid_keys ) ) {
-        return $input;
-    } else {
-        return '';
-    }
+function eggnews_sanitize_site_layout( $input ) {
+	$valid_keys = array(
+		'fullwidth_layout' => __( 'Fullwidth Layout', 'eggnews' ),
+		'boxed_layout'     => __( 'Boxed Layout', 'eggnews' )
+	);
+	if ( array_key_exists( $input, $valid_keys ) ) {
+		return $input;
+	} else {
+		return '';
+	}
+}
+
+// site layout
+function eggnews_sanitize_site_skin( $input ) {
+	$valid_keys = array(
+		'default' => __( 'Default Skin', 'eggnews' ),
+	);
+	if ( array_key_exists( $input, $valid_keys ) ) {
+		return $input;
+	} else {
+		return '';
+	}
 }
 
 // Switch option (enable/disable)
-function eggnews_enable_switch_sanitize($input) {
-    $valid_keys = array(
-        'enable' => __( 'Enable', 'eggnews' ),
-        'disable' => __( 'Disable', 'eggnews' )
-        );
-    if ( array_key_exists( $input, $valid_keys ) ) {
-        return $input;
-    } else {
-        return '';
-    }
+function eggnews_enable_switch_sanitize( $input ) {
+	$valid_keys = array(
+		'enable'  => __( 'Enable', 'eggnews' ),
+		'disable' => __( 'Disable', 'eggnews' )
+	);
+	if ( array_key_exists( $input, $valid_keys ) ) {
+		return $input;
+	} else {
+		return '';
+	}
 }
 
 //switch option (show/hide)
 function eggnews_show_switch_sanitize( $input ) {
-    $valid_keys = array(
-            'show'  => __( 'Show', 'eggnews' ),
-            'hide'  => __( 'Hide', 'eggnews' )
-        );
-    if ( array_key_exists( $input, $valid_keys ) ) {
-        return $input;
-    } else {
-        return '';
-    }
+	$valid_keys = array(
+		'show' => __( 'Show', 'eggnews' ),
+		'hide' => __( 'Hide', 'eggnews' )
+	);
+	if ( array_key_exists( $input, $valid_keys ) ) {
+		return $input;
+	} else {
+		return '';
+	}
 }
 
 //Archive page layout
-function eggnews_sanitize_archive_layout($input) {
-    $valid_keys = array(
-        'classic'   => __( 'Classic Layout', 'eggnews' ),
-        'columns'   => __( 'Columns Layout', 'eggnews' )
-        );
-    if ( array_key_exists( $input, $valid_keys ) ) {
-        return $input;
-    } else {
-        return '';
-    }
+function eggnews_sanitize_archive_layout( $input ) {
+	$valid_keys = array(
+		'classic' => __( 'Classic Layout', 'eggnews' ),
+		'columns' => __( 'Columns Layout', 'eggnews' )
+	);
+	if ( array_key_exists( $input, $valid_keys ) ) {
+		return $input;
+	} else {
+		return '';
+	}
 }
 
 //Post/Page sidebar layout
 function eggnews_page_layout_sanitize( $input ) {
-    $valid_keys = array(
-            'right_sidebar' => get_template_directory_uri() . '/inc/admin/assets/images/right-sidebar.png',
-            'left_sidebar' => get_template_directory_uri() . '/inc/admin/assets/images/left-sidebar.png',
-            'no_sidebar' => get_template_directory_uri() . '/inc/admin/assets/images/no-sidebar.png',
-            'no_sidebar_center' => get_template_directory_uri() . '/inc/admin/assets/images/no-sidebar-center.png'
-        );
-    if ( array_key_exists( $input, $valid_keys ) ) {
-        return $input;
-    } else {
-        return '';
-    }
+	$valid_keys = array(
+		'right_sidebar'     => get_template_directory_uri() . '/inc/admin/assets/images/right-sidebar.png',
+		'left_sidebar'      => get_template_directory_uri() . '/inc/admin/assets/images/left-sidebar.png',
+		'no_sidebar'        => get_template_directory_uri() . '/inc/admin/assets/images/no-sidebar.png',
+		'no_sidebar_center' => get_template_directory_uri() . '/inc/admin/assets/images/no-sidebar-center.png'
+	);
+	if ( array_key_exists( $input, $valid_keys ) ) {
+		return $input;
+	} else {
+		return '';
+	}
 }
 
 //Footer widget columns
 function eggnews_footer_widget_sanitize( $input ) {
-    $valid_keys = array(
-            'column1'   => __( 'One Column', 'eggnews' ),
-            'column2'   => __( 'Two Columns', 'eggnews' ),
-            'column3'   => __( 'Three Columns', 'eggnews' ),
-            'column4'   => __( 'Four Columns', 'eggnews' )
-        );
-    if ( array_key_exists( $input, $valid_keys ) ) {
-        return $input;
-    } else {
-        return '';
-    }
+	$valid_keys = array(
+		'column1' => __( 'One Column', 'eggnews' ),
+		'column2' => __( 'Two Columns', 'eggnews' ),
+		'column3' => __( 'Three Columns', 'eggnews' ),
+		'column4' => __( 'Four Columns', 'eggnews' )
+	);
+	if ( array_key_exists( $input, $valid_keys ) ) {
+		return $input;
+	} else {
+		return '';
+	}
 }
 
 //Related posts type
 function eggnews_sanitize_related_type( $input ) {
-    $valid_keys = array(
-            'category'   => __( 'by Category', 'eggnews' ),
-            'tag'   => __( 'by Tags', 'eggnews' )
-        );
-    if ( array_key_exists( $input, $valid_keys ) ) {
-        return $input;
-    } else {
-        return '';
-    }
+	$valid_keys = array(
+		'category' => __( 'by Category', 'eggnews' ),
+		'tag'      => __( 'by Tags', 'eggnews' )
+	);
+	if ( array_key_exists( $input, $valid_keys ) ) {
+		return $input;
+	} else {
+		return '';
+	}
 }
--- inc/admin/assets/panels/general-panel.php
+++ inc/admin/assets/panels/general-panel.php
@@ -11,82 +11,82 @@ add_action( 'customize_register', 'eggnews_general_settings_register' );
 
 function eggnews_general_settings_register( $wp_customize ) {
 
-	$wp_customize->get_section( 'title_tagline' )->panel = 'eggnews_general_settings_panel';
-    $wp_customize->get_section( 'title_tagline' )->priority = '3';
-    $wp_customize->get_section( 'colors' )->panel = 'eggnews_general_settings_panel';
-    $wp_customize->get_section( 'colors' )->priority = '4';
-    $wp_customize->get_section( 'background_image' )->panel = 'eggnews_general_settings_panel';
-    $wp_customize->get_section( 'background_image' )->priority = '5';
-    $wp_customize->get_section( 'static_front_page' )->panel = 'eggnews_general_settings_panel';
-    $wp_customize->get_section( 'static_front_page' )->priority = '6';
+	$wp_customize->get_section( 'title_tagline' )->panel        = 'eggnews_general_settings_panel';
+	$wp_customize->get_section( 'title_tagline' )->priority     = '3';
+	$wp_customize->get_section( 'colors' )->panel               = 'eggnews_general_settings_panel';
+	$wp_customize->get_section( 'colors' )->priority            = '4';
+	$wp_customize->get_section( 'background_image' )->panel     = 'eggnews_general_settings_panel';
+	$wp_customize->get_section( 'background_image' )->priority  = '5';
+	$wp_customize->get_section( 'static_front_page' )->panel    = 'eggnews_general_settings_panel';
+	$wp_customize->get_section( 'static_front_page' )->priority = '6';
 
-    /**
-     * Add General Settings Panel 
-     */
-    $wp_customize->add_panel( 
-        'eggnews_general_settings_panel',
-        array(
-            'priority'       => 3,
-            'capability'     => 'edit_theme_options',
-            'theme_supports' => '',
-            'title'          => __( 'General Settings', 'eggnews' ),
-            ) 
-        );
+	/**
+	 * Add General Settings Panel
+	 */
+	$wp_customize->add_panel(
+		'eggnews_general_settings_panel',
+		array(
+			'priority'       => 3,
+			'capability'     => 'edit_theme_options',
+			'theme_supports' => '',
+			'title'          => __( 'General Settings', 'eggnews' ),
+		)
+	);
 
-/*-----------------------------------------------*/
-    //Theme color
-    $wp_customize->add_setting(
-        'eggnews_theme_color',
-        array(
-            'default'           => '#1b651b',
-            'sanitize_callback' => 'sanitize_hex_color',
-        )
-    );
-    $wp_customize->add_control(
-        new WP_Customize_Color_Control(
-            $wp_customize,
-            'eggnews_theme_color',
-            array(
-                'label'         => __( 'Theme color', 'eggnews' ),
-                /*'description'   => __( 'Choose color to make different your website.', 'eggnews' ),*/
-                'section'       => 'colors',
-                'priority'      => 5
-            )
-        )
-    );
+	/*-----------------------------------------------*/
+	//Theme color
+	$wp_customize->add_setting(
+		'eggnews_theme_color',
+		array(
+			'default'           => '#1b651b',
+			'sanitize_callback' => 'sanitize_hex_color',
+		)
+	);
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'eggnews_theme_color',
+			array(
+				'label'    => __( 'Theme color', 'eggnews' ),
+				/*'description'   => __( 'Choose color to make different your website.', 'eggnews' ),*/
+				'section'  => 'colors',
+				'priority' => 5
+			)
+		)
+	);
 
-/*---------------------------------------------------------------------------------------------------------------*/
-    /**
-     * Website layout
-     */
-    $wp_customize->add_section(
-        'eggnews_site_layout',
-        array(
-            'title'         => __( 'Website Layout', 'eggnews' ),
-            'description'   => __( 'Choose a site to display your website more effectively.', 'eggnews' ),
-            'priority'      => 5,
-            'panel'         => 'eggnews_general_settings_panel',
-        )
-    );
-    
-    $wp_customize->add_setting(
-        'site_layout_option',
-        array(
-            'default'           => 'fullwidth_layout',
-            'sanitize_callback' => 'eggnews_sanitize_site_layout',
-        )       
-    );
-    $wp_customize->add_control(
-        'site_layout_option',
-        array(
-            'type' => 'radio',
-            'priority'    => 10,
-            'label' => __( 'Site Layout', 'eggnews' ),
-            'section' => 'eggnews_site_layout',
-            'choices' => array(
-                'fullwidth_layout' => __( 'FullWidth Layout', 'eggnews' ),
-                'boxed_layout' => __( 'Boxed Layout', 'eggnews' )
-            ),
-        )
-    );
+	/*---------------------------------------------------------------------------------------------------------------*/
+	/**
+	 * Website layout
+	 */
+	$wp_customize->add_section(
+		'eggnews_site_layout',
+		array(
+			'title'       => __( 'Website Layout', 'eggnews' ),
+			'description' => __( 'Choose a site to display your website more effectively.', 'eggnews' ),
+			'priority'    => 5,
+			'panel'       => 'eggnews_general_settings_panel',
+		)
+	);
+
+	$wp_customize->add_setting(
+		'site_layout_option',
+		array(
+			'default'           => 'fullwidth_layout',
+			'sanitize_callback' => 'eggnews_sanitize_site_layout',
+		)
+	);
+	$wp_customize->add_control(
+		'site_layout_option',
+		array(
+			'type'     => 'radio',
+			'priority' => 10,
+			'label'    => __( 'Site Layout', 'eggnews' ),
+			'section'  => 'eggnews_site_layout',
+			'choices'  => array(
+				'fullwidth_layout' => __( 'FullWidth Layout', 'eggnews' ),
+				'boxed_layout'     => __( 'Boxed Layout', 'eggnews' )
+			),
+		)
+	);
 }
