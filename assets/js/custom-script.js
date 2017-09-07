a9f59915a95b68504a103aafd38adca9b9e1fbeb
Theme Egg <themeeggofficial@gmail.com>
2017-09-07 23:04:45 +0545

    design changed

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 	});
 	//Search toggle
 	$('.header-search-wrapper .search-main').click(function () {
-		$('.search-form-main').toggleClass('active-search');
+ 		$('.search-form-main').toggleClass('active-search');
 		$('.search-form-main .search-field').focus();
 	});
 
