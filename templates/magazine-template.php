c3d8f7dd320e37f4c6fbb783fea1ae220e1017c8
Theme Egg <themeeggofficial@gmail.com>
2017-09-12 07:33:34 +0545

    template modified

--- templates/magazine-template.php
+++ templates/magazine-template.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Magazine Page
+ * Template Name: News Home Page
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
