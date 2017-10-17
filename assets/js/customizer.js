7ac2d62e5da59eed9b3ba3f1536b4ba400c2c17a
Theme Egg <themeeggofficial@gmail.com>
2017-10-17 21:25:25 +0545

    Fix - Header image issue fix

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -69,4 +69,34 @@
             $('span.ticker-caption').html(to);
         });
     });
+	// Whether a header image is available.
+	function hasHeaderImage() {
+		var image = wp.customize( 'header_image' )();
+		return '' !== image && 'remove-header' !== image;
+	}
+
+	// Whether a header video is available.
+	function hasHeaderVideo() {
+		var externalVideo = wp.customize( 'external_header_video' )(),
+			video = wp.customize( 'header_video' )();
+
+		return '' !== externalVideo || ( 0 !== video && '' !== video );
+	}
+
+	// Toggle a body class if a custom header exists.
+	$.each( [ 'external_header_video', 'header_image', 'header_video' ], function( index, settingId ) {
+		wp.customize( settingId, function( setting ) {
+			setting.bind(function() {
+				if ( hasHeaderImage() ) {
+					$( document.body ).addClass( 'has-header-image' );
+				} else {
+					$( document.body ).removeClass( 'has-header-image' );
+				}
+
+				if ( ! hasHeaderVideo() ) {
+					$( document.body ).removeClass( 'has-header-video' );
+				}
+			} );
+		} );
+	} );
 }(jQuery));
---
+++ template-parts/header/header-image.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Displays header media
+ *
+ * @package WordPress
+ * @subpackage EggNews
+ * @since 1.3.4
+ * @version 1.3.4
+ */
+
+?>
+<div class="custom-header">
+
+		<div class="custom-header-media">
+			<?php the_custom_header_markup(); ?>
+		</div>
+
+</div><!-- .custom-header -->
